31ee99d64c5142d5dc63aa0961654e7d652570f1
Pragati Verma <verma.pragati3104@gmail.com>
2024-11-06 20:20:45 +0530

    nodemailer updated

--- File Upload App/models/file.js
+++ File Upload App/models/file.js
@@ -1,4 +1,6 @@
 const mongoose =  require('mongoose');
+require('dotenv').config();
+const nodemailer = require('nodemailer');
 
 const FileSchema = new mongoose.Schema({
     name:{
@@ -17,6 +19,31 @@ const FileSchema = new mongoose.Schema({
         type:String,
         required:true,
     }
+})
+
+FileSchema.post("save" , async function (doc) {
+    try{
+        console.log("entry saved" , doc)
+
+        //transporter
+        let transporter = nodemailer.createTransport({
+            host : process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            }
+        })
+        let info = await transporter.sendMail({
+            from:`File Upload app`,
+            to: doc.email,
+            subject:"New file uploaded on cloudinary",
+            html:`<h2>FILE UPLOADED</h2>`
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
+    
 })
 
 module.exports = mongoose.model("File" , FileSchema);
